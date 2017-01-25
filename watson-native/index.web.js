749af2ecca148638b4b39bb538da43027f16d42e
Jenny Chan <jrc337@cornell.edu>
2017-01-25 18:27:31 -0500

    deleted react native portion

--- watson-native/index.web.js
+++
@@ -1,5 +0,0 @@
-import React from 'react';
-import App from './src/web/app.web';
-import ReactNative from 'react-native';
-
-ReactNative.render(<App />, document.getElementById('react-native-everywhere'));
