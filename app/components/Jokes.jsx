485e2a0d695c3c1fe339c57002fb64bc97b9a0a3
Jenny Chan <jrc337@cornell.edu>
2017-01-26 17:51:59 -0500

    refactored route

--- app/components/Jokes.jsx
+++ app/components/Jokes.jsx
@@ -1,17 +1,56 @@
 import React, { Component } from 'react';
+import axios from 'axios'
+import { VictoryBar, VictoryChart, VictoryAxis, VictoryTheme } from 'victory';
 
 export default class BonesJokes extends Component {
-  componentDidMount() {
+  constructor(props){
+    super(props);
+    this.state = {
+      personality: [],
+      tone: []
+    }
+  }
 
+  componentDidMount() {
+    axios.get('/api/watson')
+      .then(res => res.data)
+      .then(data => {
+        this.setState({
+          personality: data.personality,
+          tone: data.tone
+        });
+      })
+      .catch(err => console.error(err));
   }
 
   render() {
 
     return (
       <div>
-        HELLO WORLD
+        <VictoryChart
+          // adding the material theme provided with Victory
+          theme={VictoryTheme.material}
+          domainPadding={20}
+        >
+          <VictoryAxis
+            tickValues={this.state.personality.map(value => value.quality)}
+            style={{
+              tickLabels: {fontSize: 2, padding: 10}
+            }}
+          />
+          <VictoryAxis
+            dependentAxis
+            tickFormat={(x) => (`${x * 100}%`)}
+          />
+          <VictoryBar
+            data={this.state.personality}
+            x="score"
+            y="quality"
+            width={1000}
+            horizontal={true}
+          />
+        </VictoryChart>
       </div>
     )
   }
 }
-
--- server/watson.js
+++ server/watson.js
@@ -1,111 +1,111 @@
 const SpeechToTextV1 = require('watson-developer-cloud/speech-to-text/v1');
-var PersonalityInsightsV3 = require('watson-developer-cloud/personality-insights/v3')
-var ToneAnalyzerV3 = require('watson-developer-cloud/tone-analyzer/v3');
-const db = require('../db')
-const Recording = db.model('recordings')
-
+const PersonalityInsightsV3 = require('watson-developer-cloud/personality-insights/v3');
+const ToneAnalyzerV3 = require('watson-developer-cloud/tone-analyzer/v3');
 const fs = require('fs');
-const path = require('path')
+const path = require('path');
+
+const db = require('../db');
+const Recording = db.model('recordings');
+
+function convertText(obj) {
+    let text = '';
+    obj.results.forEach(result => {
+        text += result.alternatives[0].transcript
+    });
+    return text;
+}
+
+function convertPersonalityData(obj) {
+    let data = [];
+    const keys = Object.keys(obj);
+    for (let key in obj) {
+        if (key === 'personality') {
+            for (let i = 0; i < obj.personality.length; i++) {
+                const arrayA = obj.personality[i].children;
+                arrayA.forEach(child => data.push({
+                    quality: child.name,
+                    score: child.percentile
+                }));
+            }
+        } else if (key === 'needs' || key === 'values') {
+            const arrayB = obj[key];
+            arrayB.forEach(child => data.push({
+                quality: child.name,
+                score: child.percentile
+            }));
+        }
+    }
+    return data;
+}
+
+function convertToneData(obj) {
+    let data = [];
+    obj.document_tone.tone_categories.forEach(category => {
+        category.tones.forEach(tone => data.push({
+            quality: tone.tone_name,
+            score: tone.score
+        }))
+    });
+    return data;
+}
 
 module.exports = require('express').Router()
-  .get('/', (req, res, next) => {
-      console.log(__dirname)
-     var speech_to_text = new SpeechToTextV1({
-  username: '3f0eb922-3f9e-4f20-a075-7664696537f6',
-  password: '6lY3qlbd00Ah'
-});
+    .get('/', (req, res, next) => {
+        const speech_to_text = new SpeechToTextV1({
+            username: '3f0eb922-3f9e-4f20-a075-7664696537f6',
+            password: '6lY3qlbd00Ah'
+        });
 
-var params = {
-  // From file
-  audio: fs.createReadStream(path.join(__dirname, 'LailaFlac.flac')),
-  content_type: 'audio/flac',
-  continuous:true,
-  model: 'en-US_NarrowbandModel'
-};
+        const params = {
+            // From file
+            audio: fs.createReadStream(path.join(__dirname, 'LailaFlac.flac')),
+            content_type: 'audio/flac',
+            continuous: true,
+            model: 'en-US_NarrowbandModel'
+        };
 
-speech_to_text.recognize(params, function(err, resp) {
-  if (err)
-    console.log(err);
-  else
-    var x = resp
-    var textconvert = function(obj){
-      var text = ''
-      obj.results.forEach(function(result){
-        text += result.alternatives[0].transcript
-      })
-      return text
-    }
-    x = textconvert(x)
+        speech_to_text.recognize(params, (err, resp) => {
+            if (err) console.log(err);
+            else {
+              resp = convertText(resp);
+              Recording.create({ text: resp })
+                  .then(recording => {
+                      const personality_insights = new PersonalityInsightsV3({
+                          username: '825e1257-f5af-43d4-8afa-79d6fa99d4aa',
+                          password: 'qK2HGTmsrYdO',
+                          version_date: '2016-10-19'
+                      });
 
-    Recording.create({text: x})
-    .then(function(recording){
-      var personality_insights = new PersonalityInsightsV3({
-        username: '825e1257-f5af-43d4-8afa-79d6fa99d4aa',
-        password: 'qK2HGTmsrYdO',
-        version_date: '2016-10-19'
-      });
+                      personality_insights.profile({
+                          text: recording.text,
+                          consumption_preferences: true
+                      }, (err, response) => {
+                          if (err) console.log(err);
+                          else {
+                            response = convertPersonalityData(response);
+                            recording.update({ personality: response })
+                                .then(recording => {
+                                    const tone_analyzer = new ToneAnalyzerV3({
+                                        username: '973b3ea5-4733-4fd3-a5af-f1edb7ddd485',
+                                        password: '1E3Qbkhx3RKI',
+                                        version_date: '2016-05-19'
+                                    });
 
-      personality_insights.profile({
-        text: recording.text,
-        consumption_preferences: true
-        },
-        function (err, response) {
-          if (err)
-            console.log('error:', err);
-          else
-            function v(obj){
-              var data = []
-              var keys = Object.keys(obj)
-              for(var key in obj){
-                if(key === 'personality'){
-                  for(var i= 0;i<obj.personality.length;i++){
-                    var array = obj.personality[i].children
-                    array.forEach(function(child){
-                      data.push({quality: child.name, score: child.percentile})
-                    })
-                    }
-                }
-                else if(key === 'needs' || key ==='values'){
-                  var array2 = obj[key]
-                  array2.forEach(function(child){ data.push({quality: child.name, score: child.percentile})})
-                }
-                    }
-                    return data
-                  }
+                                    tone_analyzer.tone({ text: recording.text }, (err, tone) => {
+                                        if (err) console.log(err);
+                                        else {
+                                            tone = convertToneData(tone);
 
-                  response = v(response)
-
-                  recording.update({personality: response})
-                  .then(function(recording){
-                    var tone_analyzer = new ToneAnalyzerV3({
-                      username: '973b3ea5-4733-4fd3-a5af-f1edb7ddd485',
-                      password: '1E3Qbkhx3RKI',
-                      version_date: '2016-05-19'
-                    });
-
-                    tone_analyzer.tone({ text: recording.text },
-                      function(err, tone) {
-                        if (err)
-                          console.log(err);
-                        else
-                          function h(obj){
-                            var data2 = []
-                            obj.document_tone.tone_categories.forEach(function(category){
-                              category.tones.forEach(function(tone){
-                                data2.push({quality: tone.tone_name, score: tone.score })
+                                            recording.update({ tone: tone })
+                                                .then(recording => {
+                                                    res.send(recording)
+                                                })
+                                        }
+                                    });
                                 })
-                              })
-                                  return data2
-                                }
-                                tone = h(tone)
-                                recording.update({tone: tone})
-                                .then(function(recording){
-                                  res.send(recording)
-                                })
-                    });
+                          }
+                      });
                   })
-      });
+              }
+        })
     })
-})
-})
-
