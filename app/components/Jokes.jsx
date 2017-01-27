516ef12a8d532549759e3c6abf8bfd9426c6fcc1
Anuj Shah <ashah812@gmail.com>
2017-01-27 09:23:20 -0500

    added routes for admin user and tested fs

--- app/components/Jokes.jsx
+++ app/components/Jokes.jsx
@@ -12,44 +12,21 @@ export default class BonesJokes extends Component {
   }
 
   componentDidMount() {
-    axios.get('/api/watson')
-      .then(res => res.data)
-      .then(data => {
-        this.setState({
-          personality: data.personality,
-          tone: data.tone
-        });
-      })
-      .catch(err => console.error(err));
+    // axios.get('/api/watson')
+    //   .then(res => res.data)
+    //   .then(data => {
+    //     this.setState({
+    //       personality: data.personality,
+    //       tone: data.tone
+    //     });
+    //   })
+    //   .catch(err => console.error(err));
   }
 
   render() {
 
     return (
       <div>
-        <VictoryChart
-          // adding the material theme provided with Victory
-          theme={VictoryTheme.material}
-          domainPadding={20}
-        >
-          <VictoryAxis
-            tickValues={this.state.personality.map(value => value.quality)}
-            style={{
-              tickLabels: {fontSize: 2, padding: 10}
-            }}
-          />
-          <VictoryAxis
-            dependentAxis
-            tickFormat={(x) => (`${x * 100}%`)}
-          />
-          <VictoryBar
-            data={this.state.personality}
-            x="score"
-            y="quality"
-            width={1000}
-            horizontal={true}
-          />
-        </VictoryChart>
       </div>
     )
   }
--- server/users.js
+++ server/users.js
@@ -2,11 +2,12 @@
 
 const db = require('APP/db')
 const User = db.model('users')
+const Recording = db.model('recordings')
 
 const {mustBeLoggedIn, forbidden,} = require('./auth.filters')
 
 module.exports = require('express').Router()
-	.get('/', forbidden('only admins can list users'), (req, res, next) => 
+	.get('/', forbidden('only admins can list users'), (req, res, next) =>
 		User.findAll()
 		.then(users => res.json(users))
 		.catch(next))
@@ -14,7 +15,33 @@ module.exports = require('express').Router()
 		User.create(req.body)
 		.then(user => res.status(201).json(user))
 		.catch(next))
-	.get('/:id', mustBeLoggedIn, (req, res, next) => 
+	.get('/:id', mustBeLoggedIn, (req, res, next) =>
 		User.findById(req.params.id)
 		.then(user => res.json(user))
+		.catch(next))
+	.get('/:id/allrecordings', mustBeLoggedIn, (req, res, next) =>
+		Recording.findAll()
+		.then(recordings => res.json(recordings))
+		.catch(next))
+	.get('/:id/weekrecordings', mustBeLoggedIn, (req, res, next) =>
+		Recording.findAll({where:{}})
+		.then(recordings => res.json(recordings))
+		.catch(next))
+	.get('/:id/monthrecordings', mustBeLoggedIn, (req, res, next) =>
+		Recording.findAll({where:{}})
+		.then(recordings => res.json(recordings))
+		.catch(next))
+	.get('/:id/allrecordings/average', mustBeLoggedIn, (req, res, next) =>
+		Recording.findAll()
+		.then(recordings => res.json(recordings))
+		.catch(next))
+	.get('/:id/weekrecordings/average', mustBeLoggedIn, (req, res, next) =>
+		Recording.findAll({where:{}})
+		.then(recordings => res.json(recordings))
+		// here we will map the text of all the recordings into one big string and send over to watson. Then send over parsed info.
+		.catch(next))
+	.get('/:id/monthrecordings/average', mustBeLoggedIn, (req, res, next) =>
+		Recording.findAll({where:{}})
+		.then(recordings => res.json(recordings))
+		// here we will map the text of all the recordings into one big string and send over to watson. Then send over parsed info.
 		.catch(next))
