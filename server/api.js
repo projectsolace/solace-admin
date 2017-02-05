430fe70a13af2b83892410ddb888e302526dba7c
Jenny Chan <jrc337@cornell.edu>
2017-02-05 13:48:35 -0500

    removed configuration.json file

--- server/api.js
+++ server/api.js
@@ -4,7 +4,6 @@ const db = require('APP/db')
 const api = module.exports = require('express').Router()
 
 api
-  .get('/heartbeat', (req, res) => res.send({ok: true,}))
   .use('/auth', require('./auth'))
   .use('/users', require('./users'))
   .use('/watson', require('./watson'))
