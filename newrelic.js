ec779773348623b555a3009963aa9bdfb08f1b0c
Jenny Chan <jrc337@cornell.edu>
2017-02-13 19:26:28 -0500

    Add New Relic

---
+++ newrelic.js
@@ -0,0 +1,26 @@
+'use strict'
+
+/**
+ * New Relic agent configuration.
+ *
+ * See lib/config.defaults.js in the agent distribution for a more complete
+ * description of configuration variables and their potential values.
+ */
+exports.config = {
+  /**
+   * Array of application names.
+   */
+  app_name: ['solace-admin'],
+  /**
+   * Your New Relic license key.
+   */
+  license_key: 'e70119a89c64f5075dbea6183f12cdba54d87e9a',
+  logging: {
+    /**
+     * Level at which to log. 'trace' is most useful to New Relic when diagnosing
+     * issues with the agent, 'info' and higher will impose the least overhead on
+     * production applications.
+     */
+    level: 'info'
+  }
+}
